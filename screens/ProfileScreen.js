12029dba9772296edaef2d91967961a739fb4e2d
Yevhen Piotrovskyi <milkikomasiko@gmail.com>
2018-11-24 16:07:54 +0200

    improve profile page

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { View, Text, Button, Alert, AsyncStorage } from 'react-native';
+import { View, Text, Button, Alert, AsyncStorage, SectionList, StyleSheet } from 'react-native';
 import { setToken } from '../api';
 // import { ExpoConfigView } from '@expo/samples';
+import { Constants } from 'expo';
 
 export default class ProfileScreen extends React.Component {
   static navigationOptions = ({ navigation }) => ({
@@ -26,18 +27,117 @@ export default class ProfileScreen extends React.Component {
     ),
   });
 
-  handleAddMoney() {
-    this.props.navigation.navigate('Payment');
-  }
-
   render() {
-    /* Go ahead and delete ExpoConfigView and replace it with your
-     * content, we just wanted to give you a quick view of your config */
+    const { manifest } = Constants;
+
+    const sections = [
+      { data: [{ value: 'login' }], title: 'Login' },
+      { data: [{ value: 'AF1212GB' }], title: 'Car number' },
+      { data: [{ value: manifest.version }], title: 'version' },
+    ];
+
     return (
-      <View>
-        <Text>profile here</Text>
-        <Button onPress={() => this.handleAddMoney()} title="add money" color="blue" />
-      </View>
+      <SectionList
+        style={styles.container}
+        renderItem={this._renderItem}
+        renderSectionHeader={this._renderSectionHeader}
+        stickySectionHeadersEnabled={true}
+        keyExtractor={(item, index) => index}
+        sections={sections}
+      />
     );
   }
+
+  _renderSectionHeader = ({ section }) => {
+    return <SectionHeader title={section.title} />;
+  };
+
+  _renderItem = ({ item }) => {
+    if (item.type === 'color') {
+      return <SectionContent>{item.value && <Color value={item.value} />}</SectionContent>;
+    } else {
+      return (
+        <SectionContent>
+          <Text style={styles.sectionContentText}>{item.value}</Text>
+        </SectionContent>
+      );
+    }
+  };
 }
+
+const SectionHeader = ({ title }) => {
+  return (
+    <View style={styles.sectionHeaderContainer}>
+      <Text style={styles.sectionHeaderText}>{title}</Text>
+    </View>
+  );
+};
+
+const SectionContent = props => {
+  return <View style={styles.sectionContentContainer}>{props.children}</View>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  titleContainer: {
+    paddingHorizontal: 15,
+    paddingTop: 15,
+    paddingBottom: 15,
+    flexDirection: 'row',
+  },
+  titleIconContainer: {
+    marginRight: 15,
+    paddingTop: 2,
+  },
+  sectionHeaderContainer: {
+    backgroundColor: '#fbfbfb',
+    paddingVertical: 8,
+    paddingHorizontal: 15,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: '#ededed',
+  },
+  sectionHeaderText: {
+    fontSize: 14,
+  },
+  sectionContentContainer: {
+    paddingTop: 8,
+    paddingBottom: 12,
+    paddingHorizontal: 15,
+  },
+  sectionContentText: {
+    color: '#808080',
+    fontSize: 14,
+  },
+  nameText: {
+    fontWeight: '600',
+    fontSize: 18,
+  },
+  slugText: {
+    color: '#a39f9f',
+    fontSize: 14,
+    backgroundColor: 'transparent',
+  },
+  descriptionText: {
+    fontSize: 14,
+    marginTop: 6,
+    color: '#4d4d4d',
+  },
+  colorContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  colorPreview: {
+    width: 17,
+    height: 17,
+    borderRadius: 2,
+    marginRight: 6,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: '#ccc',
+  },
+  colorTextContainer: {
+    flex: 1,
+  },
+});
