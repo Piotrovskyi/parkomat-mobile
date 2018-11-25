b970c90e1f59caaa2d697c8b13c9a2e120bedf82
Yevhen Piotrovskyi <milkikomasiko@gmail.com>
2018-11-25 14:35:37 +0200

    tst8

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ EXPO_PASSWORD=$2
 
 docker run -i --rm \
 -v ${pwd}:/app -w /app --user $(id -u):$(id -g) \
-node: 10 \
-sh -c 'npm i -g expo && expo login -u ${EXPO_USERNAME} -p ${EXPO_PASSWORD} --non-interactive && npm run build'
+node:10 \
+sh -c 'npm i && npm i -g expo && expo login -u ${EXPO_USERNAME} -p ${EXPO_PASSWORD} --non-interactive && expo publish'
